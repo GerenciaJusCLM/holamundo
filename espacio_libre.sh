4ce7e7296b7cd651b166de7a5e33672f2d7bb234
Gerinfo <gerencia.justica.clm@gmail.com>
2024-11-12 13:10:13 +0000

    Update espacio_libre.sh

--- espacio_libre.sh
+++ espacio_libre.sh
@@ -2,4 +2,4 @@
 
 # Muestra el espacio en disco de todos los sistemas de archivos
 echo "Espacio en disco disponible en el sistema:"
-df -h --output=source,fstype,size,used,avail,pcent,target | column -t
+#df -h --output=source,fstype,size,used,avail,pcent,target | column -t
